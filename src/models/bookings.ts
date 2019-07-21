cde124c4e533d735fdf4d73d8096e0af38d09440
Abdullah Fitri <ftk2431@gmail.com>
2019-07-21 23:59:03 +0800

    update 'bookings' model

--- src/models/bookings.ts
+++ src/models/bookings.ts
@@ -1,5 +1,9 @@
 export interface Bookings {
+  key?: string;
   address: string;
+  streetName: string;
+  postCode: number;
+  city: string;
   eventType: string;
   eventDate: Date;
   mobileNumber: string;
