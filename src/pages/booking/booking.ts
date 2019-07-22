81a179c57bb59bf2b4301e52a4020adcc2546c14
Abdullah Fitri <ftk2431@gmail.com>
2019-07-22 10:53:28 +0800

    fix booking - loadingCtrl and alertCtrl display

--- src/pages/booking/booking.ts
+++ src/pages/booking/booking.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 import { Bookings } from '../../models/bookings';
+import { LoginPage } from '../login/login';
+import { AuthService } from '../../providers/auth-service/auth.service';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+// import { AngularFireDatabase } from 'angularfire2/database';
+import { BookingDetailsService } from '../../providers/booking-details/booking-details.service';
 
 /**
  * Generated class for the BookingPage page.
@@ -16,28 +21,67 @@ import { Bookings } from '../../models/bookings';
 })
 export class BookingPage {
 
-  bookings = {} as Bookings;
+  bookingForm: FormGroup;
+  bookingError: string;
 
+  bookingsDetails = {} as Bookings;
   date_today;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,
+  private auth: AuthService, fb: FormBuilder, private bookings: BookingDetailsService, private loadingCtrl: LoadingController)
+  {
+    this.bookingForm = fb.group({
+      address: ['', Validators.compose([Validators.required])],
+      streetName: ['', Validators.compose([Validators.required])],
+      postCode: ['', Validators.compose([Validators.required])],
+      city: ['', Validators.compose([Validators.required])],
+      eventType: ['', Validators.compose([Validators.required])],
+      eventDate: ['dd/mm/yyyy', Validators.compose([Validators.required])],
+      mobileNumber: ['', Validators.compose([Validators.required])]
+    });
     this.date_today = new Date().toISOString();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad BookingPage');
-  }
-
-  // showDatePicker() {
-  //     this.datePicker.show({
-  //     date: new Date(),
-  //     mode: 'date',
-  //     androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
-  //   }).then(
-  //     date => console.log('Got date: ', date),
-  //     err => console.log('Error occurred while getting date: ', err)
-  //   );
+  // ionViewWillLoad () {
+  //   let loading = this.loadingCtrl.create({
+  //     content: 'Please wait...'
+  //   });
+  //   loading.present();
+  //   setTimeout(() => {
+  //     loading.dismiss();
+  //   }, 5000);
   // }
 
+   logOut(): void {
+    this.auth.logOutUser().then( response => {
+        let alert = this.alertCtrl.create({
+          title: 'Success!',
+          subTitle: 'You have been logout from Moment',
+          buttons: ['OK']
+        });
+        alert.present();
+        this.navCtrl.setRoot(LoginPage);
+    });
+   }
 
+   makeBooking (bookingsDetails: Bookings) {
+    //
+    const result = this.bookings.makeBooking(bookingsDetails).then(ref => {
+      let loading = this.loadingCtrl.create({
+          content: 'Please wait while we confirm your booking...'
+        });
+        loading.present();
+        setTimeout(() => {
+          if (result) {
+            let alert = this.alertCtrl.create({
+                title: 'Success!',
+                subTitle: 'Booking has been made',
+                buttons: ['OK']
+              });
+            alert.present();
+          }
+          loading.dismiss();
+        }, 5000);
+    })
+  }
 }
