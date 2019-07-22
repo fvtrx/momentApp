a2b14afa060fdb7ff34ae2ce8d474ef43969e35a
Abdullah Fitri <ftk2431@gmail.com>
2019-07-22 10:53:43 +0800

    add 'profile' tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { HomePage } from "../home/home";
 // import { ProfilePage } from "../profile/profile";
 import { BookingPage } from "../booking/booking";
 import { ActivityPage } from "../activity/activity";
+import { ProfilePage } from "../profile/profile";
 // import { NavController } from "ionic-angular";
 
 @Component({
@@ -13,4 +14,5 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = BookingPage;
   tab3Root = ActivityPage;
+  tab4Root = ProfilePage;
 }
